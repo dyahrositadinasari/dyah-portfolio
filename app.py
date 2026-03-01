d540484f4f93379afbaf8de21e7daec49241ccd8
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2026-03-01 20:27:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-
+with open("Resume Dyah Dinasari.pdf", "rb") as file:
+    st.download_button(
+        label="Download Resume",
+        data=file,
+        file_name="Dyah_Dinasari_Resume.pdf",
+        mime="application/pdf"
+    )
