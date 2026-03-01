a7fc3eb20911fdb9bdf28038cf00641fdfee0f44
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2026-03-01 21:28:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ with open("Resume Dyah Dinasari.pdf", "rb") as file:
     st.download_button(
         label="Download Resume",
         data=file,
-        file_name="Dyah_Dinasari_Resume.pdf",
+        file_name="Resume_Dyah_Dinasari.pdf",
         mime="application/pdf"
     )
 
